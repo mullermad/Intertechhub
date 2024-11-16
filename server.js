3415ea9e691a05fd9a64c619902511d92c4a85a3
muller <muller@muller.muller>
2024-11-16 16:12:17 +0300

    initial commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+// Load environment variables from .env file
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+// Home Route
+app.get("/", (req, res) => {
+  res.status(200).send("Home Route");
+});
+
+// Route: /name
+app.get("/name", (req, res) => {
+  res.status(200).send("Muluken Demis");
+});
+
+// Route: /hobby
+app.get("/hobby", (req, res) => {
+  res.status(200).json({ hobby: ["Soccer", "Watching Movies"] });
+});
+
+// Route: /dream
+app.get("/dream", (req, res) => {
+  res.status(200).send("Stay focused, keep learning, and achieve greatness!");
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
